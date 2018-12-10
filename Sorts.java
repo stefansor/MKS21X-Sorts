4dee3ae5c635d81b4f5794b4dca362a9b1d1edd9
stefansor <ssorobay00@stuy.edu>
2018-12-10 11:03:29 -0500

    must add insertionSort

--- Sorts.java
+++ Sorts.java
@@ -1,13 +1,13 @@
 public class Sorts{
   public static void selectionSort(int[] ary){
     for(int i = 0; i < ary.length; i++){
-      int smallest = ary[i];
-      int swap = 0;
+      int smallest = ary[i];//saves value of first term to compare to
+      int swap = 0;//saved value of item that will be swapped
       for(int j = i + 1; j < ary.length; j++){
         if(ary[j] < smallest){
-          smallest = ary[j];
-          swap = ary[i];
-          ary[j] = swap;
+          smallest = ary[j];//saving first value
+          swap = ary[i];//saving items value
+          ary[j] = swap;//swapping
           ary[i] = smallest;
         }
       }
@@ -17,14 +17,20 @@ public class Sorts{
   public static void bubbleSort(int[] data){
     for(int i = 0; i < data.length; i++){
       for(int j = 0; j < data.length - i - 1; j++){
-        int first = data[j];
+        int first = data[j]; //saving values that are being checked
         int second = data[j + 1];
-        if(first > second){
-          data[j] = second;
+        if(first > second){//comparing values to see if swap is needed
+          data[j] = second;//swapping
           data[j + 1]= first;
           //System.out.println(printArray(data));
         }
       }
+    }
+  }
+  public static void insertionSort(int[] ary){
+    int largest = ary[0]; //comparing last to new term
+    for(int i = 1; i < ary.length; i++){
+      
     }
   }
 
